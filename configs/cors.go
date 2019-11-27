302492b7a55314387f2dbbcb9fab6b6b3cb0e1d7
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-11-27 00:51:41 -0300

    add cors && add body parse application/json

--- configs/cors.go
+++ configs/cors.go
@@ -1,7 +1,21 @@
 package configs
 
-import "log"
+import (
+	"github.com/go-chi/cors"
+)
 
-func init() {
-	log.Println("cors: apllied")
+// Cors : apply cors
+func Cors() *cors.Cors {
+	cors := cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+
+	return cors
 }
--- configs/routes.go
+++ configs/routes.go
@@ -7,13 +7,20 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
 
 // InitRoutes : init all routes from API
 func InitRoutes(db *sql.DB) *chi.Mux {
 	router := chi.NewRouter()
 
+	router.Use(Cors().Handler)
+	router.Use(middleware.RequestID)
 	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+	router.Use(render.SetContentType(render.ContentTypeJSON))
+
 	router.Mount("/api/v1/users", user.Routes(db))
 	router.Mount("/api/v1/operations", operation.Routes(db))
 
